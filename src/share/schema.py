82a2831d79e7765b3face7489be5260f6c5242a2
masashi <s.masashi.shukatsu@gmail.com>
2026-05-16 11:37:49 +0900

    fix: Change variable names and add docstring
    Change 'retrieved_summaries' to 'candidate_tables'
    
    Change 'selected_schemas' to 'selected_table_schemas'

--- src/share/schema.py
+++ src/share/schema.py
@@ -15,34 +15,25 @@ class MultiQuery(BaseModel):
 
 
 class GraphState(TypedDict):
+    """ワークフローの状態を表すクラス。
+    Attributes:
+        question (str): ユーザーの質問
+        queries (list[str]): LLMが生成する検索クエリのリスト
+        candidate_tables(list[str]): ベクトル検索後の候補テーブルリスト
+        selected_table_schemas (list[dict[str, Any]]): 抽出されたテーブル名と詳細情報のmdファイルの文字列の辞書のリスト
+        generated_sql (str): LLMが生成したSQL
+        error_history (list[str]): エラー履歴
+        execution_result (dict[str, Any]): SQLの実行結果
+        final_answer (str): ユーザーに渡す回答
+        retry_count (int): リトライ回数
+    """
+
     question: str
     queries: list[str]
-    retrieved_summaries: list[dict[str, Any]]
-    selected_schemas: list[dict[str, Any]]
+    candidate_tables: list[str]
+    selected_table_schemas: list[dict[str, Any]]
     generated_sql: str
     error_history: Annotated[list[str], operator.add]
     execution_result: dict[str, Any]
     final_answer: str
     retry_count: int
-
-
-"""
-例)
-retrieved_summaries = [
-    {
-        "text": "users",
-        "table_name": ""
-    },
-]
-selected_schemas = [
-  {
-    "table_name": "users",
-    "logical_name": "顧客マスタ",
-    "ddl": "CREATE TABLE orders (order_id INT, user_id INT, amount DECIMAL, order_date DATE)",
-    "column_descriptions": [
-        {"name": "status", "description": "0: 未入金, 1: 入金済, 9: キャンセル"}
-    ],
-  },
-]
-
-"""
