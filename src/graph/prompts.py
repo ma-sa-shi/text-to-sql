4a49b482a356180443b9e59f69ca7d5277664d2b
masashi <s.masashi.shukatsu@gmail.com>
2026-05-20 21:08:26 +0900

    feat: Rename generate sql prompt and add retry to the prompt
    Rename 'query_sql_prompt' to 'generate_sql_prompt'
    
    Modify the prompt to accept error messages and executed SQL as input
    
    Add notes on outputting only SQL and instructions for resolving errors to prompt

--- src/graph/prompts.py
+++ src/graph/prompts.py
@@ -7,10 +7,19 @@ query_gen_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-query_sql_prompt = ChatPromptTemplate.from_messages(
+generate_sql_prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "テーブル情報を参照して質問が求めるデータを取得するSQLを生成して"),
-        ("human", "質問: {question}\n\nテーブル情報: {selected_table_schemas}"),
+        (
+            "system",
+            "テーブル情報を参照して質問が求めるデータを取得するSQLを生成して"
+            "【注意事項】\n"
+            "出力は解説やMarkdownの記号を含めず実行可能なSQLのみを出力すること\n"
+            "「エラーメッセージ」と「実行したSQL」が提供されている場合は、「実行したSQL」に間違いがあることを意味する。「エラーメッセージ」から質問の意図を満たすように修正したSQLを生成して",
+        ),
+        (
+            "human",
+            "質問: {question}\n\nテーブル情報: {selected_table_schemas}\n\nエラーメッセージ: {error_message}\n\n実行したSQL: {generated_sql}",
+        ),
     ]
 )
 
